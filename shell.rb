458d78db42253e1c9f76cab3ee300f3af18c2e76
Noah Lackstein <noah@lackstein.com>
2016-04-05 18:12:50 -0400

    Screw fancy interactive shells. Just start a server

--- shell.rb
+++ shell.rb
@@ -1,13 +1,17 @@
 #!/home/hus/.rvm/rubies/ruby-2.3.0/bin/ruby
 
 require 'open3'
+require 'logger'
 
+log_name = "#{Time.now.to_i}.txt"
+logger = Logger.new "/var/www/html/logs/#{log_name}"
 port = rand(1000) + 20000
-data = {:out => [], :err => []}
 
 puts <<~START
 You can connect to the server by running:
   java -cp bin boardgame.Client student_player.StudentPlayer hus.lackstein.com #{port}
+A log of the game is available at:
+  http://hus.lackstein.com/logs/#{log_name}
 START
 
 # see: http://stackoverflow.com/a/1162850/83386
@@ -16,9 +20,7 @@ Open3.popen3("cd ~/hus-ai/; java -cp bin boardgame.Server -ng -p #{port}") do |s
   { :out => stdout, :err => stderr }.each do |key, stream|
     Thread.new do
       until (raw_line = stream.gets).nil? do
-        parsed_line = Hash[:timestamp => Time.now, :line => "#{raw_line}"]
-        # append new lines
-        data[key].push parsed_line
+        Logger.info raw_line
 
         puts raw_line
       end
